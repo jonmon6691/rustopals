1c066f71db1c1b8fac83631a47799d09073a3338
jonmon6691 <jonmon6691@gmail.com>
2024-02-27 07:57:51 +0100

    Libraries aren't cheating!

--- chal_1_1/src/main.rs
+++ chal_1_1/src/main.rs
@@ -1,3 +1,30 @@
+use base64::Engine;
+
 fn main() {
-    println!("Hello, world!");
+    easy_mode();
+    hard_mode();
+}
+
+fn easy_mode() {
+    let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    let input_bytes = hex::decode(input).unwrap();
+    let output = base64::engine::general_purpose::STANDARD.encode(input_bytes);
+    println!("{output}");
+}
+
+fn hard_mode() {
+    let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
+    let input_bytes = hex_decode(input).unwrap();
+    let output = base64_encode(input_bytes);
+    println!("{output}");
+}
+
+fn hex_decode(data: &str) -> Result<Vec<u8>, hex::FromHexError>
+{
+    Ok(vec![1])
+}
+
+fn base64_encode(data: Vec<u8>) -> String
+{
+    "Hard mode isn't done yet".to_string()
 }
