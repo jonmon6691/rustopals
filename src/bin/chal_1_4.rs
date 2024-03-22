18d03690221160b6432f9f8af4d0e3ab338137ce
jonmon6691 <jonmon6691@gmail.com>
2024-03-22 15:47:11 -0700

    Why use many line when one do trick

--- src/bin/chal_1_4.rs
+++ src/bin/chal_1_4.rs
@@ -5,15 +5,10 @@ fn main() {
     println!("https://cryptopals.com/sets/1/challenges/4 - Detect single-character XOR");
 
     // Get the ciphertext strings directly from the cryptopals site. If you are reading this in 50 years and the URL is long dead; you have my condolences, cryptopals was really cool back in the day
-    let ct_file= ureq::get("https://cryptopals.com/static/challenge-data/4.txt")
-        .call().unwrap()
-        .into_string().unwrap();
-
-    // Split the long body string into lines
-    let ct_lines: Vec<&str> = ct_file.split('\n').collect();
-
     // One line to rule them all
-    let (_score, key, plaintext) = ct_lines.iter()
+    let (_score, key, plaintext) = ureq::get("https://cryptopals.com/static/challenge-data/4.txt")
+        .call().unwrap()
+        .into_string().unwrap().split('\n')
         .map(|x| rustopals::hex_decode(x).unwrap())
         .map(|x| rustopals::crack_single_byte_xor(&x))
         .sorted_by_key(|(score, _, _)| *score)
