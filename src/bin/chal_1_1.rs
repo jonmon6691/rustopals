78f6d06de3d07c27eed133c9e830bfcb1eafe206
Jon Wallace <jonmon6691@gmail.com>
2024-03-24 14:07:24 -0700

    terse means terse

--- src/bin/chal_1_1.rs
+++ src/bin/chal_1_1.rs
@@ -1,23 +1,4 @@
-use base64::Engine;
-
 fn main() {
-    println!("https://cryptopals.com/sets/1/challenges/1 - Convert hex to base64");
-    
-    easy_mode();
-    hard_mode();
-}
-
-fn easy_mode() {
-    let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-    let input_bytes = hex::decode(input).unwrap();
-    let output = base64::engine::general_purpose::STANDARD.encode(input_bytes);
+    let output = rustopals::base64_encode(rustopals::hex_decode("49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"));
     println!("{output}");
 }
-
-fn hard_mode() {
-    let input = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-    let input_bytes = rustopals::hex_decode(input);
-    let output = rustopals::base64_encode(input_bytes);
-    println!("{output}");
-}
-
--- src/bin/chal_1_2.rs
+++ src/bin/chal_1_2.rs
@@ -1,17 +1,16 @@
+use std::iter::zip;
+
 fn main() {
     println!("https://cryptopals.com/sets/1/challenges/2 - Fixed XOR");
 
     let input_bytes = rustopals::hex_decode("1c0111001f010100061a024b53535009181c");
     let key_bytes = rustopals::hex_decode("686974207468652062756c6c277320657965");
-    let key_str = String::from_utf8(key_bytes.clone()).unwrap();
-    println!("Key: '{key_str}'");
+    println!("Key: '{}'", String::from_utf8(key_bytes.clone()).unwrap());
 
-    // Do da thing
-    let mut pt_bytes: Vec<u8> = Vec::new();
-    for (i, a) in input_bytes.iter().enumerate() {
-        pt_bytes.push(*a ^ key_bytes.get(i % key_bytes.len()).unwrap());
-    }
+    let pt = String::from_utf8(
+        zip(input_bytes.iter(), key_bytes.iter())
+        .map(|(a, b)| a ^ b)
+        .collect()).unwrap();
 
-    let pt_str = String::from_utf8(pt_bytes).unwrap();
-    println!("Plaintext: '{pt_str}'");
+    println!("Plaintext: '{pt}'");
 }
