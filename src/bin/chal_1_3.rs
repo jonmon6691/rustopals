302b6ef5a1dc1202dd829dac6e839d44281865c2
Jon Wallace <jonmon6691@gmail.com>
2024-03-23 18:06:55 -0700

    nested tuples were giving me a headache

--- src/bin/chal_1_3.rs
+++ src/bin/chal_1_3.rs
@@ -4,13 +4,9 @@ fn main() {
     let ct_bytes = rustopals::hex_decode("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736").unwrap();
     
     // Get the highest score
-    let (key, _score) =  rustopals::crack_single_byte_xor(&ct_bytes);
+    let answer =  rustopals::SBX::from_ciphertext(&ct_bytes);
 
     //Print the results
-    println!("Key: dec:{} hex:{:x} ascii:{}", key, key, key as char);
-    println!("Plaintext: {}", 
-        match String::from_utf8(rustopals::single_byte_xor(&ct_bytes, key)) {
-            Ok(s) => s,
-            Err(_) => String::from("[utf-8 decoding error]")
-        });
+    println!("Key: dec:{} hex:{:x} ascii:{}", answer.key, answer.key, answer.key as char);
+    println!("{}", answer.to_string());
 }
--- src/bin/chal_1_4.rs
+++ src/bin/chal_1_4.rs
@@ -6,19 +6,15 @@ fn main() {
 
     // Get the ciphertext strings directly from the cryptopals site. If you are reading this in 50 years and the URL is long dead; you have my condolences, cryptopals was really cool back in the day
     // One line to rule them all
-    let (ct_bytes, (key, _score)) = ureq::get("https://cryptopals.com/static/challenge-data/4.txt")
+    let ice = ureq::get("https://cryptopals.com/static/challenge-data/4.txt")
         .call().unwrap()
         .into_string().unwrap().split('\n')
         .map(|line| rustopals::hex_decode(line).unwrap())
-        .map(|ct| (ct.clone(), rustopals::crack_single_byte_xor(&ct)))
-        .sorted_by_key(|(_,(_, score))| *score)
+        .map(|ct| rustopals::SBX::from_ciphertext(&ct))
+        .sorted_by_key(|trial| trial.score)
         .rev().next().unwrap();
 
     //Print the results
-    println!("Key: dec:{} hex:{:x} ascii:{}", key, key, key as char);
-    println!("Plaintext: {}", 
-        match String::from_utf8(rustopals::single_byte_xor(&ct_bytes, key)) {
-            Ok(s) => s,
-            Err(_) => String::from("[utf-8 decoding error]")
-        });
+    println!("Key: dec:{} hex:{:x} ascii:{}", ice.key, ice.key, ice.key as char);
+    println!("{}", ice.to_string());
 }
