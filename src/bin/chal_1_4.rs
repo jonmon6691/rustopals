9ae25f63a72e7862acd175be50d2306e21e196b7
jonmon6691 <jonmon6691@gmail.com>
2024-03-25 17:07:33 -0700

    I sure hope I didn't take down cryptopals

--- src/bin/chal_1_4.rs
+++ src/bin/chal_1_4.rs
@@ -1,19 +1,24 @@
+use std::{fs::File, io::Read};
 use itertools::Itertools;
-use rustopals::EverythingRemainsRaw;
-use ureq;
+use rustopals::{EverythingRemainsRaw, SBX};
+
+fn do_chal() -> SBX {
+    // One line to rule them all
+    File::open("test_data/4.txt")
+        .unwrap().bytes()
+        .map(|c| c.unwrap() as char)
+        .collect::<String>()
+        .split('\n')
+        .map(|line| Vec::from_hex(line))
+        .map(|ct| rustopals::SBX::from_ciphertext(&ct))
+        .sorted_by_key(|trial| trial.score)
+        .rev().next().unwrap()
+}
 
 fn main() {
     println!("https://cryptopals.com/sets/1/challenges/4 - Detect single-character XOR");
 
-    // Get the ciphertext strings directly from the cryptopals site. If you are reading this in 50 years and the URL is long dead; you have my condolences, cryptopals was really cool back in the day
-    // One line to rule them all
-    let ice = ureq::get("https://cryptopals.com/static/challenge-data/4.txt")
-        .call().unwrap()
-        .into_string().unwrap().split('\n')
-        .map(|line| Vec::from_hex(line))
-        .map(|ct| rustopals::SBX::from_ciphertext(&ct))
-        .sorted_by_key(|trial| trial.score)
-        .rev().next().unwrap();
+    let ice = do_chal();
 
     //Print the results
     println!("Key: dec:{} hex:{:x} ascii:{}", ice.key, ice.key, ice.key as char);
@@ -22,6 +27,15 @@ fn main() {
 
 #[test]
 fn chal_1_4() {
+    let ice = do_chal();
+    assert_eq!(ice.key, !202); // Obfuscated ;)
+}
+
+/* For posterity, here's the version that took down the website (maybe) 
+use ureq;
+
+fn chal_1_4() {
+    // Get the ciphertext strings directly from the cryptopals site. If you are reading this in 50 years and the URL is long dead; you have my condolences, cryptopals was really cool back in the day
     let ice = ureq::get("https://cryptopals.com/static/challenge-data/4.txt")
         .call().unwrap()
         .into_string().unwrap().split('\n')
@@ -31,3 +45,4 @@ fn chal_1_4() {
         .rev().next().unwrap();
     assert_eq!(ice.key, !202); // Obfuscated ;)
 }
+*/
