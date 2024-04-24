90e1d3fa1ba9cdb328c8d45efd98efb706c31dbe
Jon Wallace <jonmon6691@gmail.com>
2024-04-23 20:53:00 -0700

    AES decryption CLI

--- src/bin/chal_1_7.rs
+++ src/bin/chal_1_7.rs
@@ -1,36 +1,75 @@
-use std::fs;
+use std::{fs, io::{self, stdin, stdout, Read, Write}};
 use aes::{cipher::{generic_array::GenericArray, BlockDecrypt, KeyInit}, Aes128};
 use cipher::block_padding::Pkcs7;
-use itertools::Itertools;
 use rustopals::raw::EverythingRemainsRaw;
+use clap::Parser;
 
-fn do_chal() -> Vec<u8> {
-    // Load the file, remove newlines to make one long line of b64 and decode
-    let mut raw_input = Vec::from_base64(
-        &fs::read_to_string("test_data/7.txt")
-            .expect("Error reading file")
-            .split_ascii_whitespace()
-            .join("")
-    );
-    let key = "YELLOW SUBMARINE".as_bytes();
-
-    Aes128::new(&GenericArray::from_slice(&key))
-        .decrypt_padded::<Pkcs7>(&mut raw_input)
-        .expect("Padding error in decrypted message!")
-        .to_owned()
+#[derive(Parser)]
+#[command(version, about = "Decrypts input using provided key. Writes result to output")]
+struct Args {
+    /// Input file (use '-' for STDIN)
+    #[arg(default_value_t = {"-".to_string()})]
+    input_file: String,
+    
+    /// Output file (use '-' for STDOUT)
+    #[arg(default_value_t = {"-".to_string()})]
+    output_file: String,
+    
+    /// AES Key (128-bit only)
+    #[arg(short='k', long="key")]
+    key: String,
 }
 
 fn main() {
-    println!("Chal 7!");
+    let args = Args::parse();
     
-    println!("{}", do_chal().to_string());
+    // Setup the input source
+    let source: Box<dyn Read> = if args.input_file == "-".to_owned() {
+        Box::new(stdin())
+    } else {
+        Box::new(fs::File::open(&args.input_file)
+            .expect(&format!("Couldn't open input file {}", args.input_file)))
+    };
+
+    // Setup the output source
+    let mut dest: Box<dyn Write> = if args.output_file == "-".to_owned() {
+        Box::new(stdout())
+    } else {
+        Box::new(fs::File::create(&args.output_file)
+            .expect(&format!("Couldn't open output file {}", args.output_file)))
+    };
+
+    let data = io::read_to_string(source)
+        .expect(&format!("Error reading from {}", args.input_file));
+
+    let mut ct = Vec::from_base64(&data);
+
+    let key = args.key.as_bytes();
+    assert_eq!(key.len(), 16, "Key must be 16 bytes!");
+
+    let pt = Aes128::new(&GenericArray::from_slice(&key))
+        .decrypt_padded::<Pkcs7>(&mut ct)
+        .expect("Padding error in decrypted message!");
+
+    dest.write(pt).expect("Couldn't write to output file!");
 }
 
 #[test]
 fn chal_1_7() {
     use itertools::fold;
+    use itertools::Itertools;
 
-    let pt = do_chal();
+    let mut raw_input = Vec::from_base64(&fs::read_to_string("test_data/7.txt")
+        .expect("Error reading file")
+        .split_ascii_whitespace()
+        .join(""));
+
+    let key = "YELLOW SUBMARINE".as_bytes();
+
+    let pt = Aes128::new(&GenericArray::from_slice(&key))
+        .decrypt_padded::<Pkcs7>(&mut raw_input)
+        .expect("Padding error in decrypted message!")
+        .to_owned();
 
     // Check sum
     let sum = fold(pt.iter(), 0 as usize, |a, &b| a + b as usize);
