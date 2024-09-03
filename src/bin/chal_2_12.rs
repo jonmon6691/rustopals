aa08cd18b1c7bb1015e800d7a1a58633a71cfe0d
jonmon6691 <jonmon6691@gmail.com>
2024-09-03 16:49:50 -0700

    Doing to dos

--- src/bin/chal_2_12.rs
+++ src/bin/chal_2_12.rs
@@ -1,6 +1,5 @@
 use rand::{thread_rng, Rng};
 use rustopals::{blocky::aes_128_ecb_encrypt_vec, raw::EverythingRemainsRaw};
-use std::io::Read;
 
 fn main() {
     let mut rng = thread_rng();
@@ -11,36 +10,28 @@ fn main() {
 
 fn ecb_phonebook(blackbox: impl Fn(&[u8]) -> Vec<u8>) -> Vec<u8> {
     let block_size = detect_ecb_blocksize(&blackbox, 512 / 8);
-    println!("{:?}", block_size);
+    println!("Detected block size (bytes): {:?}", block_size);
     vec![]
 }
 
+/// Passes longer and longer strings to the black box encrypter until its size jumps. The size of the jump is assumed to be the block length
 fn detect_ecb_blocksize(blackbox: impl Fn(&[u8]) -> Vec<u8>, max_bytes: usize) -> Option<usize> {
-    let mut probe: Vec<u8> = Vec::new();
-    let mut last_size: Option<usize> = None;
-    // TODO: This could probably be done with some kind of clever fold
-    for _ in 0..=max_bytes {
-        let ct_len = blackbox(&probe).len();
-        let delta = ct_len - *last_size.get_or_insert(ct_len);
+    let mut probe: Vec<u8> = Vec::with_capacity(max_bytes);
+    let min_size = blackbox(&probe).len();
+    for _ in 1..=max_bytes {
+        probe.push(0);
+        let delta = blackbox(&probe).len() - min_size;
         if delta != 0 {
             return Some(delta);
         }
-        probe.push(0);
     }
     None
 }
 
 fn channel(input: &[u8], key: [u8; 128 / 8]) -> Vec<u8> {
-    // Random length suffix of random bytes
+    // Secret suffix, we will decrypt it by attacking ECB's inherent weakness
     let suffix: Vec<u8> = Vec::from_base64("Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd24gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdXN0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK");
-
-    // TODO: Surely this is the worst possible way to do this
-    let data: Vec<u8> = input
-        .chain(suffix.as_slice())
-        .bytes()
-        .map(|x| x.unwrap())
-        .collect();
-
+    let data: Vec<u8> = Vec::from_iter(input.iter().chain(suffix.iter()).copied());
     aes_128_ecb_encrypt_vec(data, key)
 }
 
